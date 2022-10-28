d574172c0d349b32b787edbf9ee8ec428b57e97a
akdenizk <kadir__akdeniz@hotmail.com>
2022-10-28 09:27:02 +0300

    added profile id control to contract endpoint. returns contract only if it belongs to the caller profile id that is either a client or a contractor

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const { sequelize } = require('./model')
+const { getProfile } = require('./middleware/getProfile')
 
 const app = express();
 
@@ -13,12 +13,14 @@ app.set('models', sequelize.models)
  * FIX ME!
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
+app.get('/contracts/:id', getProfile, async (req, res) => {
+  const { Contract } = req.app.get('models')
+  const { id } = req.params
+  const contract = await Contract.findOne({ where: { id } })
+  const profile = req.profile;
+  if (profile.id !== contract.ClientId && profile.id !== contract.ContractorId) return res.status(401).end();
+  if (!contract) return res.status(404).end()
+  res.json(contract)
 })
 
 module.exports = app;
